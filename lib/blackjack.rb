fb5a792b92460f76c6160ac02cb61fe9b35bb0cc
Trevor Jameson <tjameson15@gmail.com>
2018-07-07 20:45:23 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -23,8 +23,7 @@ def end_game(total)
 end
 
 def initial_round
-  sum = deal_card() + deal_card()
-  return display_card_total(sum)
+  return display_card_total(deal_card() + deal_card())
 end
 
 def hit?(number)
